3e9eece0520992e0db46f286e45629111f83fd97
Zach Preston <zachattack6089@gmail.com>
2025-10-12 18:45:13 -0700

    Added database integration.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 const cors = require('cors')
 const express = require('express')
+const pg = require('pg')
 
 const app = express()
 app.use(cors())
 
-app.get('/status', (_, res) => {
-    res.send({
-        status: 'Hello world!',
+app.get('/status', async (_, res) => {
+    const client = new pg.Client({
+        host: process.env['DB_HOST'],
+        port: process.env['DB_PORT'],
+        database: process.env['DB_DATABASE'],
+        user: process.env['DB_USER'],
+        password: process.env['DB_PASSWORD'],
     })
+    await client.connect()
+
+    const query_res = await client.query(`
+        select message
+        from Status
+        limit 1
+    `)
+    const status = query_res.rows[0]['message']
+
+    res.send({ status })
 })
 
 app.listen(process.env.PORT ?? 8081, () => {
