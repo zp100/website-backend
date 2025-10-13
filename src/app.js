ec075d102d34c9102403c897e4f90ede8b0e565e
Zach Preston <zachattack6089@gmail.com>
2025-10-12 18:59:31 -0700

    test

--- src/app.js
+++ src/app.js
@@ -6,25 +6,23 @@ const app = express()
 app.use(cors())
 
 app.get('/status', async (_, res) => {
-    res.send(process.env)
+    const client = new pg.Client({
+        host: process.env['DB_HOST'],
+        port: process.env['DB_PORT'],
+        database: process.env['DB_DATABASE'],
+        user: process.env['DB_USER'],
+        password: process.env['DB_PASSWORD'],
+    })
+    await client.connect()
 
-    // const client = new pg.Client({
-    //     host: process.env['DB_HOST'],
-    //     port: process.env['DB_PORT'],
-    //     database: process.env['DB_DATABASE'],
-    //     user: process.env['DB_USER'],
-    //     password: process.env['DB_PASSWORD'],
-    // })
-    // await client.connect()
+    const query_res = await client.query(`
+        select message
+        from Status
+        limit 1
+    `)
+    const status = query_res.rows[0]['message']
 
-    // const query_res = await client.query(`
-    //     select message
-    //     from Status
-    //     limit 1
-    // `)
-    // const status = query_res.rows[0]['message']
-
-    // res.send({ status })
+    res.send({ status })
 })
 
 app.listen(process.env.PORT ?? 8081, () => {
