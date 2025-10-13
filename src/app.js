7042a80d2b2f34dcabc3d049fde619165e432fe9
Zach Preston <zachattack6089@gmail.com>
2025-10-12 19:23:24 -0700

    Revert "test"
    This reverts commit 832292eceaccbbdd4d460147cb7332fba4f703c6.

--- src/app.js
+++ src/app.js
@@ -13,23 +13,23 @@ const pool = new pg.Pool({
 })
 
 app.get('/status', async (_, res) => {
-    const client = await pool.connect();
-    try {
-        const query_res = await client.query(`
-            select message
-            from Status
-            limit 1
-        `)
-        const status = query_res.rows[0]['message']
-        res.send({ status })
-    } catch (e) {
-        console.error(e)
-        res.send({
-            status: 'Database error :(',
-        })
-    } finally {
-        client.release()
-    }
+    const client = new pg.Client({
+        host: process.env['DB_HOST'],
+        port: process.env['DB_PORT'],
+        database: process.env['DB_DATABASE'],
+        user: process.env['DB_USER'],
+        password: process.env['DB_PASSWORD'],
+    })
+    await client.connect()
+
+    const query_res = await client.query(`
+        select message
+        from Status
+        limit 1
+    `)
+    const status = query_res.rows[0]['message']
+
+    res.send({ status })
 })
 
 app.listen(process.env.PORT ?? 8081, () => {
