ff1d684dab4ea9248e033bcd9977df7ec8309347
Zach Preston <zachattack6089@gmail.com>
2025-10-12 19:03:56 -0700

    Switched to Vercel code example.

--- src/app.js
+++ src/app.js
@@ -5,24 +5,31 @@ const pg = require('pg')
 const app = express()
 app.use(cors())
 
+const pool = new pg.Pool({
+    connectionString: process.env['DATABASE_URL'],
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
+
 app.get('/status', async (_, res) => {
-    const client = new pg.Client({
-        host: process.env['DB_HOST'],
-        port: process.env['DB_PORT'],
-        database: process.env['DB_DATABASE'],
-        user: process.env['DB_USER'],
-        password: process.env['DB_PASSWORD'],
-    })
-    await client.connect()
-
-    const query_res = await client.query(`
-        select message
-        from Status
-        limit 1
-    `)
-    const status = query_res.rows[0]['message']
-
-    res.send({ status })
+    const client = await pool.connect();
+    try {
+        const query_res = await client.query(`
+            select message
+            from Status
+            limit 1
+        `)
+        const status = query_res.rows[0]['message']
+        res.send({ status })
+    } catch (e) {
+        console.error(e)
+        res.send({
+            status: 'Database error :(',
+        })
+    } finally {
+        client.release()
+    }
 })
 
 app.listen(process.env.PORT ?? 8081, () => {
