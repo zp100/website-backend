d05952cb5a38ce26facc17b59e58c274e86a5202
Zach Preston <zachattack6089@gmail.com>
2026-03-19 23:31:44 -0700

    Added some missing words to the word list.

--- src/routes/pangrams/filter.py
+++ src/routes/pangrams/filter.py
@@ -4,19 +4,18 @@ list_files = [
     'count_1w-100k.txt',
 ]
 
-def wordlen(word: str) -> int:
-    return len(word.strip())
-
 word_list = set()
 for list_file in list_files:
     with open(f"{dir}{list_file}", 'r') as file:
-        new_words = set(file.readlines())
+        file_words = [ word.strip(' \n\t$+^&!%') for word in file.readlines() ]
+        new_words = set([ word for word in file_words if len(word) >= 3 ])
+
     if len(word_list) > 0:
         word_list = word_list.intersection(new_words)
     else:
         word_list = new_words
 
-word_list = [ word.strip('$+^&!%') for word in word_list if wordlen(word) >= 3 ]
+word_list = [ f"{word}\n" for word in word_list ]
 word_list.extend([ 'pangram\n', 'pangrams\n' ])
 word_list.sort()
 
